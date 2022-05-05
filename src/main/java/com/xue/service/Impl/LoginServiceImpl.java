8d282d498ecc30fbe572b2c8307cc841944efdea
zhongwx <zhongwx@mama.cn>
2022-05-05 12:00:13 +0800

    [modify]

--- src/main/java/com/xue/service/Impl/LoginServiceImpl.java
+++ src/main/java/com/xue/service/Impl/LoginServiceImpl.java
@@ -1169,8 +1169,8 @@ public class LoginServiceImpl implements LoginService {
         byte[] photo = null;
         String student_name_get = null;
         String student_name = null;
-        Integer page_start = (page - 1) * 3;
-        Integer page_length = 3;
+        Integer page_start = (page - 1) * 7;
+        Integer page_length = 7;
         List<JSONObject> resul_list = new ArrayList<>();
         List<Message> list=null;
         List<User> users=null;
@@ -1208,7 +1208,7 @@ public class LoginServiceImpl implements LoginService {
                 id = line.getId();
                 create_time = line.getCreate_time();
                 studio = line.getStudio();
-                photo = line.getPhoto();
+//                photo = line.getPhoto();
 
                 try {
                     List<Lesson> lessons = dao.getLessonByName(student_name, studio);
@@ -1232,7 +1232,7 @@ public class LoginServiceImpl implements LoginService {
                 jsonObject.put("class_target", class_target);
                 jsonObject.put("id", id);
                 jsonObject.put("create_time", create_time);
-                jsonObject.put("photo", photo);
+//                jsonObject.put("photo", photo);
                 resul_list.add(jsonObject);
             }
 
