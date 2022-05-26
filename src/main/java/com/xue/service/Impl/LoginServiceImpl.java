7238d031794b2084a90f903758e5de47db2aeb0b
zhongwx <zhongwx@mama.cn>
2022-05-26 11:05:58 +0800

    [modify]

--- src/main/java/com/xue/service/Impl/LoginServiceImpl.java
+++ src/main/java/com/xue/service/Impl/LoginServiceImpl.java
@@ -1784,8 +1784,8 @@ public class LoginServiceImpl implements LoginService {
         String id = null;
         Integer points = 0;
         Float percent = 0.0f;
-        Integer page_start = (page - 1) * 4;
-        Integer page_length = 4;
+        Integer page_start = (page - 1) * 3;
+        Integer page_length = 3;
         List<Message> list_student = null;
         List<JSONObject> resul_list = new ArrayList<>();
         if(student_name.equals("all")){
