b08083918ab3540fc52a717b798f270dca8fab6e
zhongwx <zhongwx@mama.cn>
2022-06-06 09:45:52 +0800

    [modify]

---
+++ src/main/java/com/xue/entity/model/Leave.java
@@ -0,0 +1,74 @@
+package com.xue.entity.model;
+
+
+public class Leave {
+
+    private String id;
+
+    private String student_name;
+
+    private String create_time;
+
+    private String date_time;
+
+    private String studio;
+
+    private String duration;
+
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public String getDate_time() {
+        return date_time;
+    }
+
+    public void setDate_time(String date_time) {
+        this.date_time = date_time;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
+    public String getStudent_name() {
+        return student_name;
+    }
+
+    public void setStudent_name(String student_name) {
+        this.student_name = student_name;
+    }
+
+    public String getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(String create_time) {
+        this.create_time = create_time;
+    }
+
+
+	@Override
+	public String toString() {
+		return "Leave [student_name="+ student_name + " create_time="+ create_time + "]";
+	}
+
+    
+}
