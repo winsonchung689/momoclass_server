51748cb3e48d74a00b3794678145a1eb37040376
zhongwx <zhongwx@mama.cn>
2022-09-27 14:14:20 +0800

    [modify]

--- src/main/java/com/xue/controller/LoginController.java
+++ src/main/java/com/xue/controller/LoginController.java
@@ -1089,19 +1089,15 @@ public class LoginController {
 
 	@RequestMapping("/get_file")
 	@ResponseBody
-	public ResponseEntity<byte[]> get_file(HttpServletRequest request, HttpServletResponse response) throws IOException{
+	public String get_file(HttpServletRequest request, HttpServletResponse response) throws IOException{
 		String file_name =  request.getParameter("file_name");
 		String path = System.getProperty("user.dir");
 		String p_path = path +"/uploadfiles/"+ file_name;
 		File file = new File(p_path);
 		if(file.exists()){
-			org.springframework.http.HttpHeaders headers = new HttpHeaders();
-			headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-			headers.setContentDispositionFormData("attachment", file.getName());
-			return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),headers, HttpStatus.OK);
+			return "文件存在";
 		}else{
-			System.out.println("文件不存在,请重试...");
-			return null;
+			return "文件不存在";
 		}
 	}
 
