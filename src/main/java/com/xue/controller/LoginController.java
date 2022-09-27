525cc6a8907d469b393e43bd2e0d0ed2147397ee
zhongwx <zhongwx@mama.cn>
2022-09-27 10:29:53 +0800

    [modify]

--- src/main/java/com/xue/controller/LoginController.java
+++ src/main/java/com/xue/controller/LoginController.java
@@ -15,6 +15,8 @@ import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -27,6 +29,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -1873,6 +1877,20 @@ public class LoginController {
 		return "push massage successfully";
 	}
 
+
+	@Configuration
+	public class UploadFilePathConfig extends WebMvcConfigurerAdapter {
+
+		@Value("${file.staticAccessPath}")
+		private String staticAccessPath;
+		@Value("${file.uploadFolder}")
+		private String uploadFolder;
+
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			registry.addResourceHandler(staticAccessPath).addResourceLocations("file:" + uploadFolder);
+		}
+	}
 
 }
 	
