a1f1e17319971f71b2b9e0002b6590609a8f27ea
zhongwx <zhongwx@mama.cn>
2022-09-27 14:23:15 +0800

    [modify]

--- src/main/java/com/xue/SpringbootLoginApplication.java
+++ src/main/java/com/xue/SpringbootLoginApplication.java
@@ -4,6 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
@@ -13,5 +17,13 @@ public class SpringbootLoginApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootLoginApplication.class, args);
 	}
+
+	@Bean
+	public ConfigurableServletWebServerFactory webServerFactory() {
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		factory.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> connector.setProperty("relaxedQueryChars", "|{}[]\\"));
+		return factory;
+	}
+
 
 }
