fcec229dd8163db278995f9a589fd6fcb6a42478
zhongwx <zhongwx@mama.cn>
2023-02-02 14:55:26 +0800

    [modify]

--- src/main/java/com/xue/SpringbootLoginApplication.java
+++ src/main/java/com/xue/SpringbootLoginApplication.java
@@ -40,7 +40,7 @@ public class SpringbootLoginApplication {
 		SpringApplication.run(SpringbootLoginApplication.class, args);
 	}
 
-	@Scheduled(cron = "*/5 * * * * ?")
+	@Scheduled(cron = "0 */5 * * * ?")
 	public void sendClassRemind(){
 		loginService.sendClassRemind();
 	}
