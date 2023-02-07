0c59b08f20d7a8d3c442b98fb7432908451c3d21
zhongwx <zhongwx@mama.cn>
2023-02-07 16:54:41 +0800

    [modify]

--- src/main/java/com/xue/repository/dao/UserMapper.java
+++ src/main/java/com/xue/repository/dao/UserMapper.java
@@ -315,7 +315,7 @@ public interface UserMapper {
 
     int updateUserExpired(String role,String studio,String role_old);
 
-    List<Lesson> getLessonByName(String student_name,String studio);
+    List<Lesson> getLessonByName(String student_name,String studio,String subject);
 
     int updateLessonPoint(Lesson lesson);
 
