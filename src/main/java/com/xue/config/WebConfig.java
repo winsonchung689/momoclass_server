fe192e5cc77a7dda1696b3135ae938b508363ca6
zhongwx <zhongwx@mama.cn>
2023-02-09 09:37:59 +0800

    [modify]

--- src/main/java/com/xue/config/WebConfig.java
+++ src/main/java/com/xue/config/WebConfig.java
@@ -14,6 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // /home/file/**为前端URL访问路径 后面 file:xxxx为本地磁盘映射
-        registry.addResourceHandler("/file/**").addResourceLocations("file:" + "/data/uploadVideo" + "/");
+        registry.addResourceHandler("/file/**").addResourceLocations("file:" + "/data/");
     }
 }
