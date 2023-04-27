cefd68d4af6977e0dddcc4ebc951bb9d11a940f1
zhongwx <zhongwx@mama.cn>
2023-04-27 11:29:39 +0800

    [add push api]

--- src/main/java/com/xue/service/WebPushService.java
+++ src/main/java/com/xue/service/WebPushService.java
@@ -3,6 +3,7 @@ package com.xue.service;
 import nl.martijndwars.webpush.Notification;
 import nl.martijndwars.webpush.PushService;
 import nl.martijndwars.webpush.Subscription;
+import org.apache.http.HttpResponse;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.jose4j.lang.JoseException;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,10 +18,13 @@ import java.util.concurrent.ExecutionException;
 @Service
 public class WebPushService {
 
-    @Value("${vapid.public.key}")
-    private String publicKey;
-    @Value("${vapid.private.key}")
-    private String privateKey;
+//    @Value("${vapid.public.key}")
+//    private String publicKey;
+//    @Value("${vapid.private.key}")
+//    private String privateKey;
+
+    private static final String publicKey = "BP75YB6apr3U36uUoAGd_oEF4pK3QLu4RQl5jKA7SBvjPs5ssoQzVZKccSqKH-PXBgB5AAp_F4knCx3QRR9Pavg";
+    private static final String privateKey = "6ZAkoZHBvfPRq-0KLIK2ePLZ6HBmpOWVWae2DEuz0Lg";
 
     private PushService pushService;
 
@@ -34,11 +38,14 @@ public class WebPushService {
         return publicKey;
     }
 
-    public void sendNotification(Subscription subscription, String messageJson) {
+    public String sendNotification(Subscription subscription, String messageJson) {
         try {
-            pushService.send(new Notification(subscription, messageJson));
+            HttpResponse httpResponse = pushService.send(new Notification(subscription, messageJson));
+            int statusCode = httpResponse.getStatusLine().getStatusCode();
+            return String.valueOf(statusCode);
         } catch (GeneralSecurityException | IOException | JoseException | ExecutionException | InterruptedException e) {
             e.printStackTrace();
+            return null;
         }
     }
 
