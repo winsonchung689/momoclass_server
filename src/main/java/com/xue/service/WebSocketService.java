18e4f906c05d7efeda57c06c4a8dc65d4cb1520e
zhongwx <zhongwx@mama.cn>
2023-05-03 10:34:59 +0800

    [modify]

--- src/main/java/com/xue/service/WebSocketService.java
+++ src/main/java/com/xue/service/WebSocketService.java
@@ -11,7 +11,7 @@ import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 
-@ServerEndpoint(value = "/websocket/{userName}")
+@ServerEndpoint(value = "/websocket")
 @Component
 public class WebSocketService {
 
