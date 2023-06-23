20f7b8c3332f729da864847958c1a0c7cbb7ddd4
zhongwx <zhongwx@mama.cn>
2023-06-23 13:50:42 +0800

    [modify]

--- src/main/java/com/xue/service/Impl/LoginServiceImpl.java
+++ src/main/java/com/xue/service/Impl/LoginServiceImpl.java
@@ -1834,8 +1834,10 @@ public class LoginServiceImpl implements LoginService {
                     List<Book> books = dao.getBookByStudio(studio);
                     if(books.size()>0){
                         String mark = books.get(0).getMark();
-                        String pay_type = mark.substring(mark.length() - 2);
+                        String pay_type = mark.split("_")[0].substring(mark.length() - 2);
+                        String all_days = mark.split("_")[1];
                         jsonObject.put("pay_type", pay_type);
+                        jsonObject.put("all_days", all_days);
                     }
                 }
 
