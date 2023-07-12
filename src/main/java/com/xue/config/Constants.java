ef85f7cb84b558b08580d3f7f888f6c78b7ea152
zhongwx <zhongwx@mama.cn>
2023-07-12 22:12:59 +0800

    [modify]

--- src/main/java/com/xue/config/Constants.java
+++ src/main/java/com/xue/config/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
     public static final String secret = "fa2eb83ce18350429132b8c5ac11f43f";
 
     public static final String appid_2b = "wxc61d8f694d20f083";
-    public static final String secret_2b = "ed083522ff79ac7dad24e115aecfbc08";
+    public static final String secret_2b = "8e23d28264b79b9d45f0c46d12d3f166";
 
 }
