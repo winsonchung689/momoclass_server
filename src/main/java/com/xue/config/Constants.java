36cf80bc8343cc269950df19a6c742ea81902fba
zhongwx <zhongwx@mama.cn>
2023-07-12 22:06:20 +0800

    [modify]

--- src/main/java/com/xue/config/Constants.java
+++ src/main/java/com/xue/config/Constants.java
@@ -2,7 +2,7 @@ package com.xue.config;
 
 public class Constants {
     public static final String appid = "wxa3dc1d41d6fa8284";
-    public static final String secret = "f2c191273540906cbc74e67d0b8fdd2a";
+    public static final String secret = "fa2eb83ce18350429132b8c5ac11f43f";
 
     public static final String appid_2b = "wxc61d8f694d20f083";
     public static final String secret_2b = "ed083522ff79ac7dad24e115aecfbc08";
