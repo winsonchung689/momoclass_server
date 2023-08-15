6f2edd1391c69084b837b15da6f5bf39c079964f
zhongwx <zhongwx@mama.cn>
2023-08-15 09:17:14 +0800

    [modify]

--- src/main/java/com/xue/repository/dao/UserMapper.java
+++ src/main/java/com/xue/repository/dao/UserMapper.java
@@ -276,7 +276,6 @@ public interface UserMapper {
 
     int updateRestaurantNickName(RestaurantUser restaurantUser);
 
-
     int updateRestaurantLogo(RestaurantUser restaurantUser);
 
     int updateUserDelete(User user);
