de98bcae7513852c6e33d16b7ffeee327c2f1e3b
zhongwx <zhongwx@mama.cn>
2023-08-23 11:06:27 +0800

    [modify]

--- src/main/java/com/xue/config/Constants.java
+++ src/main/java/com/xue/config/Constants.java
@@ -1,19 +1,6 @@
 package com.xue.config;
 
 public class Constants {
-    public static final String appid = "wxa3dc1d41d6fa8284";
-    public static final String secret = "fa2eb83ce18350429132b8c5ac11f43f";
 
-    public static final String appid_2b = "wxc61d8f694d20f083";
-    public static final String secret_2b = "8e23d28264b79b9d45f0c46d12d3f166";
-
-    public static final String official_appid = "wxc79a69144e4fd233";
-    public static final String official_secret = "3a0e2c3067c1df58bd92e4788371baeb";
-
-    public static final String order_appid = "wx80d88494062937dd";
-    public static final String order_secret = "62cd844281fb25cf1b229aa771de9e8a";
-
-    public static final String book_appid = "wxc967c1cdf284df42";
-    public static final String book_secret = "7f39d5274a588fb6cc681d72ae029571";
 
 }
