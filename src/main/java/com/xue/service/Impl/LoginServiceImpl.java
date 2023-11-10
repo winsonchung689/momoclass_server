e89b91ce958069a444fd1e5b85b67a13cad8b8d0
zhongwx <zhongwx@mama.cn>
2023-11-10 17:03:13 +0800

    [modify]

--- src/main/java/com/xue/service/Impl/LoginServiceImpl.java
+++ src/main/java/com/xue/service/Impl/LoginServiceImpl.java
@@ -930,6 +930,7 @@ public class LoginServiceImpl implements LoginService {
         }
 
         for (int i = 0; i < lessons.size(); i++) {
+            List<Schedule> schedules = new ArrayList<>();
             JSONObject jsonObject = new JSONObject();
             Lesson lesson = lessons.get(i);
             String student_name = lesson.getStudent_name();
@@ -938,19 +939,19 @@ public class LoginServiceImpl implements LoginService {
             String campus_get = lesson.getCampus();
             Float total_amount = lesson.getTotal_amount();
             Float left_amount = lesson.getLeft_amount();
-            List<Schedule> schedules = dao.getScheduleByStudent(studio_get,campus_get,subject_get,student_name);
+            schedules = dao.getScheduleByStudent(studio_get,campus_get,subject_get,student_name);
             if("已排课".equals(type) && schedules.size()>0){
                 jsonObject.put("student_name",student_name);
-                jsonObject.put("subject", subject);
-                jsonObject.put("campus", campus);
+                jsonObject.put("subject", subject_get);
+                jsonObject.put("campus", campus_get);
                 jsonObject.put("total_amount", total_amount);
                 jsonObject.put("left_amount", left_amount);
                 resul_list.add(jsonObject);
 
             }else if("未排课".equals(type) && schedules.size() == 0){
                 jsonObject.put("student_name",student_name);
-                jsonObject.put("subject", subject);
-                jsonObject.put("campus", campus);
+                jsonObject.put("subject", subject_get);
+                jsonObject.put("campus", campus_get);
                 jsonObject.put("total_amount", total_amount);
                 jsonObject.put("left_amount", left_amount);
                 resul_list.add(jsonObject);
