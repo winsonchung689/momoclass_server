55d9b430b92f4ad03dad5286fc7eb9aee55716c0
Winson <oncwnuEyoufyCrWoOUrox1LOZN9s@git.weixin.qq.com>
2024-09-05 07:07:03 +0800

    [modify]

--- src/main/java/com/xue/service/Impl/LoginServiceImpl.java
+++ src/main/java/com/xue/service/Impl/LoginServiceImpl.java
@@ -6585,14 +6585,13 @@ public class LoginServiceImpl implements LoginService {
                     Float consume_lesson = 0.0f;
                     Float consume_lesson_get = 0.0f;
                     Float lesson_gap = total_amount - left_amount;
-                    try {
-                        if(is_combine == 0){
-                            consume_lesson_get = dao.getAllSignUpByStudent(studio,subject_get,campus,student_name_all);
-                        }else if (is_combine == 1){
-                            consume_lesson_get = dao.getAllSignUpByStudentCombine(studio,campus,student_name_all);
+                    List<SignUp> signUps = dao.getSignUp(student_name,studio,subject,campus);
+                    if(signUps.size() > 0) {
+                        if (is_combine == 0) {
+                            consume_lesson_get = dao.getAllSignUpByStudent(studio, subject_get, campus, student_name_all);
+                        } else if (is_combine == 1) {
+                            consume_lesson_get = dao.getAllSignUpByStudentCombine(studio, campus, student_name_all);
                         }
-                    } catch (Exception e) {
-//                            throw new RuntimeException(e);
                     }
 
                     // 判断寻找其他关联课时
@@ -6606,13 +6605,11 @@ public class LoginServiceImpl implements LoginService {
                                 String student_name_get = lesson_re.getStudent_name();
                                 String subject_re = lesson_re.getSubject();
                                 if (!student_name_all.equals(student_name_get)) {
-                                    Float consume_lesson_re = 0.0f;
-                                    try {
-                                        consume_lesson_re = dao.getAllSignUpByStudent(studio, subject_re, campus, student_name_get);
-                                    } catch (Exception e) {
-//                                        throw new RuntimeException(e);
+                                    List<SignUp> signUps1 = dao.getSignUp(student_name,studio,subject,campus);
+                                    if(signUps1.size() > 0) {
+                                        Float consume_lesson_re = dao.getAllSignUpByStudent(studio, subject_re, campus, student_name_get);
+                                        consume_lesson_get = consume_lesson_get + consume_lesson_re;
                                     }
-                                    consume_lesson_get = consume_lesson_get + consume_lesson_re;
                                 }
                             }
                         }
@@ -7879,14 +7876,13 @@ public class LoginServiceImpl implements LoginService {
 
                 Float consume_amount = 0.0f;
                 Float consume_lesson_get = 0.0f;
-                try {
-                    if(is_combine == 0){
-                        consume_lesson_get = dao.getAllSignUpByStudent(studio,subject_get,campus,student_name);
-                    }else if(is_combine ==1 ){
-                        consume_lesson_get = dao.getAllSignUpByStudentCombine(studio,campus,student_name);
+                List<SignUp> signUps = dao.getSignUp(student_name,studio,subject_get,campus);
+                if(signUps.size() > 0) {
+                    if (is_combine == 0) {
+                        consume_lesson_get = dao.getAllSignUpByStudent(studio, subject_get, campus, student_name);
+                    } else if (is_combine == 1) {
+                        consume_lesson_get = dao.getAllSignUpByStudentCombine(studio, campus, student_name);
                     }
-                } catch (Exception e) {
-//                    throw new RuntimeException(e);
                 }
 
                 // 判断寻找其他关联课时
@@ -7900,13 +7896,11 @@ public class LoginServiceImpl implements LoginService {
                             String student_name_get = lesson_re.getStudent_name();
                             String subject_re = lesson_re.getSubject();
                             if (!student_name.equals(student_name_get)) {
-                                Float consume_lesson_re =  0.0f;
-                                try {
-                                    consume_lesson_re = dao.getAllSignUpByStudent(studio, subject_re, campus, student_name_get);
-                                } catch (Exception e) {
-//                                        throw new RuntimeException(e);
+                                List<SignUp> signUps1 = dao.getSignUp(student_name_get,studio,subject_re,campus);
+                                if(signUps1.size() > 0){
+                                    Float consume_lesson_re = dao.getAllSignUpByStudent(studio, subject_re, campus, student_name_get);
+                                    consume_lesson_get = consume_lesson_get + consume_lesson_re;
                                 }
-                                consume_lesson_get = consume_lesson_get + consume_lesson_re;
                             }
                         }
                     }
@@ -8439,15 +8433,15 @@ public class LoginServiceImpl implements LoginService {
                 Float consume_lesson_get = 0.0f;
                 Float consume_amount = 0.0f;
                 Float lesson_gap = total_amount - left_amount;
-                try {
-                    if(is_combine == 0){
-                        consume_lesson_get = dao.getAllSignUpByStudent(studio,subject,campus,student_name);
-                    }else if(is_combine == 1){
-                        consume_lesson_get = dao.getAllSignUpByStudentCombine(studio,campus,student_name);
+                List<SignUp> signUps = dao.getSignUp(student_name,studio,subject,campus);
+                if(signUps.size() > 0) {
+                    if (is_combine == 0) {
+                        consume_lesson_get = dao.getAllSignUpByStudent(studio, subject, campus, student_name);
+                    } else if (is_combine == 1) {
+                        consume_lesson_get = dao.getAllSignUpByStudentCombine(studio, campus, student_name);
                     }
-                } catch (Exception e) {
-//                    throw new RuntimeException(e);
                 }
+
 
                 // 判断寻找其他关联课时
                 if(!"no_id".equals(related_id)){
@@ -8460,13 +8454,11 @@ public class LoginServiceImpl implements LoginService {
                             String student_name_get = lesson_re.getStudent_name();
                             String subject_re = lesson_re.getSubject();
                             if (!student_name.equals(student_name_get)) {
-                                Float consume_lesson_re = 0.0f;;
-                                try {
-                                    consume_lesson_re = dao.getAllSignUpByStudent(studio, subject_re, campus, student_name_get);
-                                } catch (Exception e) {
-//                                        throw new RuntimeException(e);
+                                List<SignUp> signUps1 = dao.getSignUp(student_name_get,studio,subject_re,campus);
+                                if(signUps1.size() > 0){
+                                    Float consume_lesson_re = dao.getAllSignUpByStudent(studio, subject_re, campus, student_name_get);
+                                    consume_lesson_get = consume_lesson_get + consume_lesson_re;
                                 }
-                                consume_lesson_get = consume_lesson_get + consume_lesson_re;
                             }
                         }
                     }
