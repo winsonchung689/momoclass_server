77727e9358cf0a2c7ae053a2ab985545baea8931
Winson <oncwnuEyoufyCrWoOUrox1LOZN9s@git.weixin.qq.com>
2024-12-15 13:37:28 +0800

    [modify]

--- src/main/java/com/xue/test.java
+++ src/main/java/com/xue/test.java
@@ -1,5 +1,9 @@
 package com.xue;
 
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -27,18 +31,28 @@ public class test {
 //            long timestamp = new Date().getTime();
 //            System.out.println(end_timestamp);
 //            System.out.println(timestamp);
-        String tt = "2024-01-01";
-        LocalDate dateTime1 = LocalDate.parse(tt, formatter);
-        LocalDate dateTime2 = LocalDate.parse("2024-01-03", formatter);
-
-        while (!dateTime2.isBefore(dateTime1)){
-            System.out.println("aa:" + dateTime2.toString());
-            dateTime2 = dateTime2.minusDays(1);
-        }
-
-
+//        String tt = "2024-01-01";
+//        LocalDate dateTime1 = LocalDate.parse(tt, formatter);
+//        LocalDate dateTime2 = LocalDate.parse("2024-01-03", formatter);
+//
+//        while (!dateTime2.isBefore(dateTime1)){
+//            System.out.println("aa:" + dateTime2.toString());
+//            dateTime2 = dateTime2.minusDays(1);
+//        }
 
 
+        String model ="{\"page\":\"pages/welcome/welcome\",\"scene\":\"namewinson\",\"check_path\":true,\"env_version\":\"release\"}";
+        System.out.printf(model);
+        JSONObject queryJson = JSONObject.parseObject(model);
+
+//        try {
+////            queryJson.getJSONObject("scene").put("value", URLEncoder.encode("name=winsonbb","UTF-8"));
+//            queryJson.getJSONObject("scene").put("value", "name=winsonbb");
+//        } catch (UnsupportedEncodingException e) {
+//            throw new RuntimeException(e);
+//        }
+//        queryJson.getJSONObject("scene").put("value", "name=winsonbb");
+        System.out.println(queryJson);
 
 
     }
