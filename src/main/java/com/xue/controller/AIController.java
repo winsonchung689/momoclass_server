cebd2bb870f78b254e80bc1057dc3178d59d4047
Winson <oncwnuEyoufyCrWoOUrox1LOZN9s@git.weixin.qq.com>
2025-01-24 11:55:15 +0800

    [modify]

--- src/main/java/com/xue/controller/AIController.java
+++ src/main/java/com/xue/controller/AIController.java
@@ -56,7 +56,7 @@ public class AIController {
 			header.put("Content-Type", "application/json");
 			header.put("Authorization", "Bearer " + OPENAI_API_KEY);
 			JSONObject params = new JSONObject();
-			params.put("model", "gpt-4-vision-preview");
+			params.put("model", "gpt-4o");
 			List<JSONObject> jsonObjects = new ArrayList<>();
 			JSONObject jsonObject = new JSONObject();
 			// content
