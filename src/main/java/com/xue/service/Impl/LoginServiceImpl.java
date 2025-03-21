efec17092d9b18b68a7587ca521c7e14f675cbe2
Winson <oncwnuEyoufyCrWoOUrox1LOZN9s@git.weixin.qq.com>
2025-03-21 09:00:10 +0800

    [modify]

--- src/main/java/com/xue/service/Impl/LoginServiceImpl.java
+++ src/main/java/com/xue/service/Impl/LoginServiceImpl.java
@@ -182,7 +182,7 @@ public class LoginServiceImpl implements LoginService {
                         String[] related_id_list = related_id.split(",");
                         for(int j=0;j < related_id_list.length; j++){
                             String id_get = related_id_list[j];
-                            List<Lesson> lessons = dao.getLessonById(Integer.parseInt(id_get));
+                            List<Lesson> lessons = dao.getLessonById(id_get);
                             Lesson lesson_get = lessons.get(0);
                             String student_name_get = lesson_get.getStudent_name();
                             if(!student_name.equals(student_name_get)){
