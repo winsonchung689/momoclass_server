2203b12bfc759b15125c3e95920d4625cf67e7a7
Winson <oncwnuEyoufyCrWoOUrox1LOZN9s@git.weixin.qq.com>
2025-03-25 15:26:03 +0800

    [modify]

--- src/main/java/com/xue/repository/dao/UserMapper.java
+++ src/main/java/com/xue/repository/dao/UserMapper.java
@@ -607,6 +607,8 @@ public interface UserMapper {
 
     int updateScheduleHours(String studio,Integer hours);
 
+    int updateArrangementHours(String studio,Integer hours);
+
     List<User> getUser(String openid);
 
     List<Card> getCard(String studio,String campus,String student_name,String subject);
