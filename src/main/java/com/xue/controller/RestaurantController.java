ec231413e6633f2870f1e4fe116904afbf116a75
Winson <oncwnuEyoufyCrWoOUrox1LOZN9s@git.weixin.qq.com>
2025-04-01 11:12:12 +0800

    [modify]

--- src/main/java/com/xue/controller/RestaurantController.java
+++ src/main/java/com/xue/controller/RestaurantController.java
@@ -534,8 +534,6 @@ public class RestaurantController {
 	@RequestMapping("/freeOpenShop")
 	@ResponseBody
 	public int freeOpenShop(String openid){
-
-
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
 		String create_time = df.format(new Date());// new Date()为获取当前系统时间，也可使用当前时间戳
 		Calendar cal = Calendar.getInstance();
