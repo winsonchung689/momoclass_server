89fcc7071ed08790968d641baac0f3322acfa94f
Winson <oncwnuEyoufyCrWoOUrox1LOZN9s@git.weixin.qq.com>
2025-07-04 16:06:18 +0800

    [modify]

--- src/main/java/com/xue/entity/model/ShippingFee.java
+++ src/main/java/com/xue/entity/model/ShippingFee.java
@@ -21,6 +21,17 @@ public class ShippingFee {
 
     private String restaurant;
 
+    private String type;
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     public String getRestaurant() {
         return restaurant;
     }
