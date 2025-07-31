f1ae6a7426c505eab4dd6c22e9f701d0c3307d33
Winson <oncwnuEyoufyCrWoOUrox1LOZN9s@git.weixin.qq.com>
2025-07-31 15:07:56 +0800

    [modify]

--- src/main/java/com/xue/service/Impl/WechatPayServiceImpl.java
+++ src/main/java/com/xue/service/Impl/WechatPayServiceImpl.java
@@ -63,9 +63,9 @@ public class WechatPayServiceImpl implements WechatPayService {
             List<RestaurantUser> restaurantUsers1 = dao.getRestaurantBossByShop(studio);
             RestaurantUser restaurantUser1 = restaurantUsers1.get(0);
             int is_free = restaurantUser1.getIs_free();
-            rate = 8.0f;
+            rate = 1.0f;
             if(is_free == 1){
-                rate = 10.0f;
+                rate = 5.6f;
             }
         }
 
