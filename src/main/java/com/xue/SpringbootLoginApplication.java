5349b7e82334f9b57b99646a7df4ac1ba56660f1
Winson <oncwnuEyoufyCrWoOUrox1LOZN9s@git.weixin.qq.com>
2025-10-12 15:02:41 +0800

    [modify]

--- src/main/java/com/xue/SpringbootLoginApplication.java
+++ src/main/java/com/xue/SpringbootLoginApplication.java
@@ -57,7 +57,7 @@ public class SpringbootLoginApplication {
 		loginService.updateCoinsLevel();
 	}
 
-	@Scheduled(cron = "0 10 * * * ?")
+	@Scheduled(cron = "0 45 * * * ?")
 	public void produceClassRemindRedis(){
 		loginService.produceClassRemindRedis("none","all");
 	}
