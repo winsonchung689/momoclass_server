e4afae11210e2cf8bf1a87272797b233b9490e31
Winson <oncwnuEyoufyCrWoOUrox1LOZN9s@git.weixin.qq.com>
2025-12-20 10:20:17 +0800

    [modify]

--- src/main/java/com/xue/service/Impl/LoginServiceImpl.java
+++ src/main/java/com/xue/service/Impl/LoginServiceImpl.java
@@ -9993,25 +9993,6 @@ public class LoginServiceImpl implements LoginService {
 
             }
 
-            if("试听数".equals(type)){
-                List<Schedule> schedules = dao.getTransferAll(weekday,studio,campus);
-                for(int i=0;i< schedules.size();i++){
-                    JSONObject jsonObject = new JSONObject();
-                    Schedule schedule = schedules.get(i);
-                    String student_name = schedule.getStudent_name();
-                    String class_number = schedule.getClass_number();
-                    String duration = schedule.getDuration();
-                    String subject = schedule.getSubject();
-
-                    jsonObject.put("student_name", student_name);
-                    jsonObject.put("class_number", class_number);
-                    jsonObject.put("duration", duration);
-                    jsonObject.put("subject", subject);
-                    resul_list.add(jsonObject);
-                }
-
-            }
-
         //按日算
         }else if(weekday.length() == 10){
 
@@ -10148,6 +10129,24 @@ public class LoginServiceImpl implements LoginService {
                     jsonObject.put("give_lesson", give_lesson);
                     jsonObject.put("mark", mark);
                     jsonObject.put("create_time", create_time);
+                    resul_list.add(jsonObject);
+                }
+            }
+
+            if("试听数".equals(type)){
+                List<Schedule> schedules = dao.getTransferAll(weekday,studio,campus);
+                for(int i=0;i< schedules.size();i++){
+                    JSONObject jsonObject = new JSONObject();
+                    Schedule schedule = schedules.get(i);
+                    String student_name = schedule.getStudent_name();
+                    String class_number = schedule.getClass_number();
+                    String duration = schedule.getDuration();
+                    String subject = schedule.getSubject();
+
+                    jsonObject.put("student_name", student_name);
+                    jsonObject.put("class_number", class_number);
+                    jsonObject.put("duration", duration);
+                    jsonObject.put("subject", subject);
                     resul_list.add(jsonObject);
                 }
             }
